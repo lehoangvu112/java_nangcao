3c0bf7e2e658e73f5413bb63f739337e2936a089
LeHoangVu <LeHoangVu@LAPTOP-R5T1NVFK>
2020-10-04 15:58:31 +0700

    bi loi

---
+++ example05/src/main/java/com/lehoangvu/config/SpringConfiguration.java
@@ -0,0 +1,29 @@
+package com.lehoangvu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.lehoangvu.controller")
+public class SpringConfiguration {
+	@Bean(name="jspViewResolver")
+	
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+
---
+++ example05/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rail Way Register Form</title>
+</head>
+<body>
+    <a href="reservation/bookingForm">Click Here for reservation</a>
+</body>
+</html>
